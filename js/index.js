4ad4a63b04d74350a84a6d5f4b08008dfb3adb10
Aanand Kainth <akainth015@gmail.com>
2019-08-07 22:02:33 -0700

    Update site interface (#3)
    * Initial commit
    
    * Started top bar
    
    * Finished top left link formatting
    
    * Added main icon placeholder
    
    * Fixed mobile formatting
    
    * Added adaptive width
    
    * Added first section
    
    * First prototype
    
    * Added navigation bar animations
    
    * Separated css and html
    
    * Added easter egg
    
    * Fixed hover effect
    
    * Fixed link transitions
    
    * Fixed mobile design
    
    * Fixed mobile navbar
    
    * Added center logo
    
    * Removed text shadow
    
    * Removed header shadow
    
    * Added fontawesome
    
    * Fixed large padding
    
    * Removed float on mobile
    
    * Removed extra bottom margin
    
    * Switched to dark mode
    
    * Added logo max-width
    
    * Added unfinished V
    
    * Fixed favicon
    
    * Fixed mobile jumps
    
    * Added new logo
    
    * Added slanted lines
    
    * Changed small styling
    
    * Removed logo margin
    
    * Fixed image margin
    
    * Changed media query trigger
    
    * Added alternating pattern
    
    * Fixed navbar links
    
    * Revert "Fixed navbar links"
    
    This reverts commit 7a63aaf339ac48d9e6249f5708d17779553a8446.
    
    * Small fix for jumping
    
    * Update files
    
    * :fire: more changes
    
    * Update link styling for desktop
    
    * Add mobile menu
    
    * Update site design (#1)
    
    * Update files
    
    * :fire: more changes
    
    * Update link styling for desktop
    
    * Add mobile menu
    
    * :lipstick: Add SVG animation
    
    * Added smooth scrolling
    
    * Moved sidebar
    
    * Removed underlines in sidebar
    
    * Added acrylic navbar
    
    * Added hero icon on mobile
    
    * Fixed a typo
    
    * Fixed link scrolling
    
    * Removed navbar transition
    
    * Added extra filter for compatibility
    
    * Changed date
    
    * Fixed date
    
    * :lipstick: Smooth scroll without JavaScript
    
    * :lipstick: hehe

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector("nav");
+const blackout = document.querySelector(".blackout");
+
+blackout.addEventListener("click", () => {
+    nav.classList.remove("open");
+    blackout.style.visibility = "hidden";
+});
+
+document.querySelector("#menu-activate").addEventListener("click", () => {
+    nav.classList.toggle("open");
+    if (nav.classList.contains("open")) {
+        blackout.style.visibility = "visible";
+    } else {
+        blackout.style.visibility = "hidden";
+    }
+    document.querySelectorAll("nav a").forEach(it => {
+        const listener = () => {
+            nav.classList.remove("open");
+            blackout.style.visibility = "hidden";
+            it.removeEventListener("click", listener);
+        };
+        it.addEventListener("click", listener);
+    });
+});
+
+document.querySelectorAll("#hero-img path").forEach(it => {
+    it.style.strokeDasharray = it.getTotalLength();
+    it.style.strokeDashoffset = it.getTotalLength();
+});
